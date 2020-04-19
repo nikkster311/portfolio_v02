e7fc43ab87f448d2f76ff1b4ed8dd67cd7c3159c
nikkster311 <26155010+nikkster311@users.noreply.github.com>
2020-04-18 23:44:29 -0700

    updates

---
+++ index.js
@@ -0,0 +1,66 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
+
+
+
+
+// modal
+
+var modalBtns = document.querySelectorAll('.portfolio__item')
+modalBtns.forEach(function(btn){
+  btn.onclick = function(){
+    var modal = btn.getAttribute('data-modal');
+
+    document.getElementById(modal ).style.display = 'block';
+  };
+});
+
+var closeBtns = document.querySelectorAll('.modal-close');
+
+closeBtns.forEach(function(btn){
+  btn.onclick = function(){
+    var modal = btn.closest('.modal').style.display = 'none';
+  }
+});
+
+window.onclick = function(e){
+  if(e.target.className == "modal") {
+    e.target.style.display = "none";
+  }
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("portfolioModalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
