8ac25051a1b607d0c22ad08118b623e152a3d56f
nikkster311 <26155010+nikkster311@users.noreply.github.com>
2020-04-19 02:28:35 -0700

    modals work

--- index.js
+++ index.js
@@ -21,11 +21,12 @@ modalBtns.forEach(function(btn){
   btn.onclick = function(){
     var modal = btn.getAttribute('data-modal');
 
-    document.getElementById(modal ).style.display = 'block';
+    document.getElementById(modal).style.display = 'block';
   };
 });
+//this sets display on onclick
 
-var closeBtns = document.querySelectorAll('.modal-close');
+var closeBtns = document.querySelectorAll('.close');
 
 closeBtns.forEach(function(btn){
   btn.onclick = function(){
@@ -39,28 +40,28 @@ window.onclick = function(e){
   }
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("portfolioModalBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // Get the modal
+// var modal = document.getElementById("myModal");
+//
+// // Get the button that opens the modal
+// var btn = document.getElementById("portfolioModalBtn");
+//
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+//
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+//
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
